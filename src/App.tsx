5156f8dc620bf2c8d89a1c68c2f93f9db1ea0fa8
khanshorif331 <khanshorif331@gmail.com>
2022-10-21 11:07:26 +0600

    rouuter setup done for different paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Login from './Components/Login'
 
 function App() {
 	return (
 		<div>
-			<h1 className="text-center">Hey ami shoorif</h1>
+			{/* <h1 className="text-center">Hey ami shoorif</h1> */}
+			<Routes>
+				<Route path="/" element={<Login></Login>}></Route>
+				<Route path="/login" element={<Login></Login>}></Route>
+				<Route path="/register" element={<Login></Login>}></Route>
+				<Route path="*" element={<Login></Login>}></Route>
+			</Routes>
 		</div>
 	)
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,16 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { store } from './Store/store'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<Provider store={store}>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
+		<BrowserRouter>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</BrowserRouter>
 	</Provider>
 )
 
